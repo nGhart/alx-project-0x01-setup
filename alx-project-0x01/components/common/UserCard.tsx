e373a6c8c5e85e166093daf61cdbd49171f4c05e
oGhartey <ondghartey@gmail.com>
2025-11-16 18:47:46 +0000

    Implement Users Card Components

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,18 @@
+import { PostProps, UserProps } from "@/interfaces";
+
+const UserCArd: React.FC<UserProps> = ({ name, id }) => {
+  return (
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold text-gray-800">{name}</h2>
+      </div>
+      <p className="text-gray-600"></p>
+      <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
+        <span>User ID: {id}</span>
+        {/* <span>Post ID: </span> */}
+      </div>
+    </div>
+  );
+};
+
+export default UserCArd;
--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -21,4 +21,15 @@ const Home: React.FC = () => {
   );
 };
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
 export default Home;
